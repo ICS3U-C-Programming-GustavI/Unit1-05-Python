c2b9de41d5ecbf3505c99add22ff7ede67a06572
Gustav Ihlenfeld <gustav.ihlenfeld@stu.ocsb.ca>
2025-02-15 16:16:01 +0000

    Added area_per_rect.py file

---
+++ area_per_rect.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Created By: Gustav I
+# Date: Feb 15, 2025
+# This program calculates the area & perimeter of a 6cm X 8cm rectangle
+
+
+def main():
+    print("For a rectangle with length 6cm and width 8cm:")
+    print()
+    print("The area is: {}cm^2".format(6 * 8))
+    print("The perimeter is: {}cm".format(2 * (6 + 8)))
+
+
+if __name__ == "__main__":
+    main()
